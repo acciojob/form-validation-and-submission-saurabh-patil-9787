a48ba2eba1781380bfc100a3c29793138cddb52a
saurabh-patil-9787 <154727373+saurabh-patil-9787@users.noreply.github.com>
2025-02-20 20:21:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+let inputName = document.getElementById('name')
+let inputMail = document.getElementById('mail')
+let submitBtn = document.getElementById('btn')
+let checkMark = document.getElementById('check')
+
+// submitBtn.disabled="true";
+
+checkMark.addEventListener('change',()=>{
+    submitBtn.disabled=!checkMark.checked
+})
+submitBtn.addEventListener('click',validateform)
+function validateform(){
+    let name = inputName.value
+    let gmail = inputMail.value
+    if(name.length<=4){
+        alert("Name should have more than 5word")
+    }
+    else if(!gmail.includes("@")){
+        alert("add @")
+    }
+}
+
